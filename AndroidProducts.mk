90da17a05c9fa2a0f4c921b0e69a8bce07a0919e
Ten-Shine <76210622+Ten-Shine@users.noreply.github.com>
2021-06-19 19:33:29 +0800

    ocn: evolutionx-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_ocn.mk
+    $(LOCAL_DIR)/evolution_ocn.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_ocn-user \
-    lineage_ocn-userdebug \
-    lineage_ocn-eng
+    evolution_ocn-user \
+    evolution_ocn-userdebug \
+    evolution_ocn-eng
