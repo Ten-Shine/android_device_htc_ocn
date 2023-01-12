Ten-Shine/android_device_htc_ocn

commit 5332b91d0e1adb3a5207588ad2d9ecc7817a3dee

ocn: Update vendor boot variable
See https://r.android.com/1457443

The variable also needs to be set earlier now since it's
read-only by the time BoardConfig.mk is read.

Groundhog day, yada yada. The CADT is strong with El Goog.

device.mk
@@ -51,6 +51,10 @@ TARGET_SCREEN_WIDTH := 1440
 # Force building a boot image. This needs to be set explicitly since Android R
 PRODUCT_BUILD_BOOT_IMAGE := true
 
+# Android R: Disable logic for new vendor_boot
+# Our devices do not support it
+PRODUCT_BUILD_VENDOR_BOOT_IMAGE := false
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \

BoardConfig.mk
@@ -18,10 +18,6 @@ BOARD_VENDOR := htc
 
 DEVICE_PATH := device/htc/ocn
 
-# Android R: Disable logic for new vendor_boot
-# Our devices do not support it
-TARGET_NO_VENDOR_BOOT := true
-
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 

commit f2bb18dbd5529fd6c3f6b0537afbd5ace3ce5642

ocn: Don't explicitly disable vendor boot
None of our devices set BOARD_BOOT_HEADER_VERSION, which must be at
least version 3 for vendor_boot to be enabled by default, as per the
AOSP change that this was originally explicitly disabled for:
https://r.android.com/1457443 - it'll stay disabled for all our
platforms without explicitly setting this to `false`.
Removing this explicit disablement will allow us to enable the feature
on newer platforms like Sagami where vendor_boot is required.

Build- and boot-tested on XQ-AU52 (PDX201 DSDS, Seine, Xperia 10 II)
running Android 12, and J9110 (Griffin DSDS, Kumano, Xperia 1) running
Android 11, no vendor_boot.img is generated and the device functions as
normal.

Fixes: 5332b91d0e1adb3a5207588ad2d9ecc7817a3dee ("ocn: Update vendor boot variable")
Signed-off-by: Marijn Suijten <marijn.suijten@somainline.org>

device.mk
@@ -51,10 +51,6 @@ TARGET_SCREEN_WIDTH := 1440
 # Force building a boot image. This needs to be set explicitly since Android R
 PRODUCT_BUILD_BOOT_IMAGE := true
 
-# Android R: Disable logic for new vendor_boot
-# Our devices do not support it
-PRODUCT_BUILD_VENDOR_BOOT_IMAGE := false
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \